d077b46c67fc25352eff18b27c35614b91639bcd
Lucy Maxim <la.maxim2018@gmail.com>
2026-03-01 16:11:03 +0000

    Removing old files that are no longer necessary

--- parse_cdc.sh
+++ parse_cdc.sh
@@ -2,43 +2,22 @@
 set -euo pipefail
 
 RAW="data/raw_data/cdc_data.csv"
-PARS="data/parsed_data"
-mkdir -p "$PARS"
+PROC="data/parsed_data"
+mkdir -p "$PROC"
 
-SEMIPARSED="$PARS/semiparsed_cdc.csv"
-FINAL="$PARS/cdc_parsed.csv"
-STATE_POP="$PARS/state_populations.csv"
+SEMIPARSED="$PROC/semiparsed_cdc.csv"
+FINAL="$PROC/cdc_parsed.csv"
+STATE_POP="$PROC/state_populations.csv"
 
-echo "state,area,age_adjusted_cancer_rate,population" > "$SEMIPARSED"
+csvcut -c stateabbr,locationname,data_value,totalpopulation,short_question_text "$RAW" \
+  | csvgrep -c short_question_text -r "cancer" \
+  | csvgrep -c short_question_text -v -r "screening" \
+  > "$SEMIPARSED"
 
-tail -n +2 "$RAW" | awk -F',' '
-BEGIN { OFS="," }
-{
-  question = tolower($20)
-  if (question ~ /cancer/ && question !~ /screening/) {
-    if ($10 != "" && $12 != "") {
-      print $2,$4,$10,$12
-    }
-  }
-}' >> "$SEMIPARSED"
+csvcut -c stateabbr,data_value,totalpopulation "$SEMIPARSED" \
+  | csvstat --mean data_value --sum totalpopulation \
+  > "$FINAL"
 
-echo "state,age_adjusted_cancer_rate,population" > "$FINAL"
+csvcut -c stateabbr,totalpopulation "$FINAL" > "$STATE_POP"
 
-tail -n +2 "$SEMIPARSED" | awk -F',' '
-{
-  state=$1
-  rate[state]+=$3
-  count[state]++
-  if ($4>pop[state]) pop[state]=$4
-}
-END {
-  OFS=","
-  for (s in rate) {
-    print s, rate[s]/count[s], pop[s]
-  }
-}' >> "$FINAL"
-
-echo "state,population" > "$STATE_POP"
-tail -n +2 "$FINAL" | awk -F',' '{print $1","$3}' >> "$STATE_POP"
-
-echo "All CDC parsing complete!"
+echo "CDC parsing complete!"
